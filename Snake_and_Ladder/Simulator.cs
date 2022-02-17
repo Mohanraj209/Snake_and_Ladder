fa67ff18251398c8389cf170b49cf07f33349764
Mohanraj209 <mohanraj1715@gmail.com>
2022-02-18 02:07:47 +0530

    Program to check for an Option

--- Snake_and_Ladder/Simulator.cs
+++ Snake_and_Ladder/Simulator.cs
@@ -19,8 +19,43 @@ namespace Snake_and_Ladder
             Random random = new Random();
             int Roll = random.Next(1, 7);
             Console.WriteLine("Dice Number: " + Roll);
+            Position += Roll;
+            Option(Roll);
+            Starting_Point();
         }
+
+        public void Option(int Roll)
+        {
+            Random random = new Random();
+            int option = random.Next(0, 3);
+            switch (option)
+            {
+                case 0:
+                    // when option is no play! 
+                    Console.WriteLine("No Play");
+                    break;
+                case 1:
+                    // when option is snake
+                    Console.WriteLine("Thought you were going to win aren't you! Snake bite");
+                    Console.WriteLine("Position lowered to " + Roll);
+                    Position -= Roll;
+                    break;
+                case 2:
+                    // when option is ladder
+                    Console.WriteLine("You are not going much further! Climb Ladder");
+                    Console.WriteLine("Position highered to " + Roll);
+                    Position += Roll;
+                    break;
+                default:
+                    Console.WriteLine("Error!");
+                    break;
+            }
+
+        }
+
 
     }
 
 }
+
+
